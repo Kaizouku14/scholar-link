439483b67bf9a1e2975836168f435197b4daaf92
kaizodevx <mandaalv72@gmail.com>
2025-08-09 22:06:01 +0800

    feat(internship): add internships header

---
+++ src/app/(app)/(app)/internships/(coordinator)/interns/_components/header.tsx
@@ -0,0 +1,18 @@
+const InternshipHeader = () => {
+  return (
+    <div className="bg-primary mt-4 rounded-lg p-6 shadow-md">
+      <div className="flex items-center gap-2">
+        <div className="space-y-1.5">
+          <h1 className="text-center text-2xl font-bold text-white md:text-start md:text-3xl">
+            Manage Internships.
+          </h1>
+          <p className="text-center text-sm font-medium text-white/90 md:text-start md:text-base">
+            Manage All the intern's in your department.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default InternshipHeader;
--- src/app/(app)/(app)/internships/(coordinator)/interns/page.tsx
+++ src/app/(app)/(app)/internships/(coordinator)/interns/page.tsx
@@ -1,9 +1,11 @@
 import PageBreadCrumb from "@/components/breadcrumbs/page-header";
+import InternshipHeader from "./_components/header";
 
 const Page = () => {
   return (
     <div className="mx-auto h-auto w-full space-y-4 px-2">
       <PageBreadCrumb currentPage="Interns" />
+      <InternshipHeader />
     </div>
   );
 };
