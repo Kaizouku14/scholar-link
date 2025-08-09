e80f31c141adff245ee41f3d6ae673d44f244733
kaizodevx <mandaalv72@gmail.com>
2025-08-09 22:07:21 +0800

    feat(internship): add internships table

---
+++ src/app/(app)/(app)/internships/(coordinator)/interns/_components/table/table.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { DataTable } from "@/components/table/data-table";
+import { api } from "@/trpc/react";
+import { DataTableSkeleton } from "@/components/table/table-skeleton";
+
+const InternshipTable = () => {
+  return (
+    <div className="w-full">
+      {!true && true ? (
+        <DataTable columns={[]} data={[]} filteredTitle={""} />
+      ) : (
+        <DataTableSkeleton />
+      )}
+    </div>
+  );
+};
+
+export default InternshipTable;
--- src/app/(app)/(app)/internships/(coordinator)/interns/page.tsx
+++ src/app/(app)/(app)/internships/(coordinator)/interns/page.tsx
@@ -1,11 +1,13 @@
 import PageBreadCrumb from "@/components/breadcrumbs/page-header";
 import InternshipHeader from "./_components/header";
+import InternshipTable from "./_components/table/table";
 
 const Page = () => {
   return (
     <div className="mx-auto h-auto w-full space-y-4 px-2">
       <PageBreadCrumb currentPage="Interns" />
       <InternshipHeader />
+      <InternshipTable />
     </div>
   );
 };
