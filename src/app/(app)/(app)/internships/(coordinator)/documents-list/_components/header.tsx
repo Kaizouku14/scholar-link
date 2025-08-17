aac9378ce0cdbe9ff993d8e404351bb827f67066
kaizodevx <mandaalv72@gmail.com>
2025-08-17 13:59:59 +0800

    feat(document-list): add header

---
+++ src/app/(app)/(app)/internships/(coordinator)/documents-list/_components/header.tsx
@@ -0,0 +1,18 @@
+const DocumentListHeader = () => {
+  return (
+    <div className="bg-primary mt-4 rounded-lg p-6 shadow-md">
+      <div className="flex items-center gap-2">
+        <div className="space-y-1.5">
+          <h1 className="text-center text-2xl font-bold text-white md:text-start md:text-3xl">
+            Manage Your Intern's Documents
+          </h1>
+          <p className="text-center text-sm font-medium text-white/90 md:text-start md:text-base">
+            Monitor and manage student documents.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DocumentListHeader;
--- src/app/(app)/(app)/internships/(coordinator)/documents-list/page.tsx
+++ src/app/(app)/(app)/internships/(coordinator)/documents-list/page.tsx
@@ -1,11 +1,12 @@
 import PageBreadCrumb from "@/components/breadcrumbs/page-header";
 import DocumentListTable from "./_components/table/table";
+import DocumentListHeader from "./_components/header";
 
 const Page = () => {
   return (
     <div className="mx-auto w-full space-y-4 px-2">
       <PageBreadCrumb currentPage="Documents" />
-
+      <DocumentListHeader />
       <DocumentListTable />
     </div>
   );
